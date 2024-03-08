79d712566a0b9b5861847a13cf75947b1bab55c3
Fyphen <fyphensub@gmail.com>
2024-03-08 07:48:13 +0000

    Add new CSS styles and HTML elements for screen layout and result display

--- web/js/play.js
+++ web/js/play.js
@@ -3,10 +3,33 @@
 const socket = io();
 
 window.onload = () => {
-	alert('IPPONグランプリを開始します。準備ができれば「OK」を押してください。');
+	alert(
+		'IPPONグランプリを開始します。準備ができれば「OK」を押してください。なお、始める前には必ずページ内のどこかをクリックしないと正常に動作しません。'
+	);
 };
 
 const themeAudio = new Audio('/sounds/theme.mp3');
+const halfLeft = document.getElementById('halfLeft');
+const halfRight = document.getElementById('halfRight');
+const screenHider = document.getElementById('screenHider');
+const resultShower = document.getElementById('resultShower');
+
+/*
+halfLeft.addEventListener('click', () => {
+	halfLeft.style.display = 'none';
+	halfRight.style.display = 'none';
+	console.log('Hoi');
+});
+
+halfRight.addEventListener('click', () => {
+	halfLeft.style.display = 'none';
+	halfRight.style.display = 'none';
+});
+*/
+
+screenHider.addEventListener('click', () => {
+	screenHider.style.display = 'none';
+});
 
 const resultText = document.getElementById('result');
 socket.on('setTheme', (theme) => {
@@ -15,7 +38,7 @@ socket.on('setTheme', (theme) => {
 	title.innerHTML = '';
 	let index = 0;
 	const regex = /<[^>]*>|[^<>]+/g;
-	const parts = theme.match(regex).map((part) => (part[0] === '<' ? part : part.split(''))); // HTMLタグはそのまま、それ以外は一文字ずつに分割
+	const parts = theme.match(regex).map((part) => (part[0] === '<' ? part : part.split('')));
 	const intervalId = setInterval(() => {
 		if (index < parts.length) {
 			if (Array.isArray(parts[index])) {
@@ -35,7 +58,18 @@ socket.on('setTheme', (theme) => {
 function getRandomInt() {
 	return Math.floor(Math.random() * (500 - 20 + 1)) + 20;
 }
+
 socket.on('results', (result) => {
+	let width = 0;
+	resultShower.style.display = 'block';
+	const intervalId = setInterval(() => {
+		if (width < 100) {
+			width++;
+			resultShower.style.width = width + '%';
+		} else {
+			clearInterval(intervalId);
+		}
+	}, 100);
 	if (result[0] === true && result[1] === true) {
 		resultText.innerHTML = 'IPPON!';
 	} else {
